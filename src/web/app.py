99d514a993f625625d84edcfae488e83859dc301
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-28 01:27:25 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -3,13 +3,4 @@ from db import get_db, close_db
 import sqlalchemy
 from logger import log
 
-app = Flask(__name__)
-app.teardown_appcontext(close_db)
-
-
-@app.route("/")
-def index():
-    
-    return "indexhhgffttggg.html"
-
-
+print("88")
