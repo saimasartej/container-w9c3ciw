e9dfbda65fb1774e43495734e4ef01b970dc99be
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-28 07:19:26 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -1,6 +1,42 @@
-from flask import Flask, render_template
-from db import get_db, close_db
-import sqlalchemy
-from logger import log
+import time
+from flask import Flask, jsonify
+from multiprocessing import Process, Value
 
-print("88")
+
+app = Flask(__name__)
+
+
+tasks = [
+   {
+      'id': 1,
+      'title': u'Buy groceries',
+      'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+      'done': False
+   },
+   {
+      'id': 2,
+      'title': u'Learn Python',
+      'description': u'Need to find a good Python tutorial on the web', 
+      'done': False
+   }
+]
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+   return jsonify({'tasks': tasks})
+
+
+def record_loop(loop_on):
+   while True:
+      if loop_on.value == True:
+         print("loop running")
+      time.sleep(1)
+
+
+if __name__ == "__main__":
+   recording_on = Value('b', True)
+   p = Process(target=record_loop, args=(recording_on,))
+   p.start()  
+   app.run(debug=True, use_reloader=False)
+   p.join()
